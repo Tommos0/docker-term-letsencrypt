bef0e955132c49f3c59d320ad01d023a42d0d14a
Tom Klaver <tomklav@gmail.com>
2018-08-14 10:40:48 +0200

    add admin email env param

--- certbot.sh
+++ certbot.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 source /etc/envvars
 DOMAIN_PARAMS="-d $(echo $SSL_DOMAINS | sed 's/,/ -d /g')"
-( set -x; certbot certonly --webroot -w /acme -n --agree-tos --email tom@klaver.com ${DOMAIN_PARAMS} --expand )
+( set -x; certbot certonly --webroot -w /acme -n --agree-tos --email ${SSL_ADMIN_EMAIL} ${DOMAIN_PARAMS} --expand )
 if [ $? -ne 0 ]; then
     echo "WARNING: Certbot failed to create certificate"
     exit 1
 else
-    cp -r -L /etc/letsencrypt/live/${DOMAIN}/*.pem /cert
+  ls /etc/letsencrypt/live
+    #cp -r -L /etc/letsencrypt/live/${DOMAIN}/*.pem /cert
 fi
